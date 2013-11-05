62b2ee07b1d2d0fb6f320616b939af364753897a
Blaine Schmeisser <BlaineSch@gmail.com>
2013-11-05 14:17:46 -0600

    Add a few more query tests, update auto config.

--- extensions/data/QueryBuilder.php
+++ extensions/data/QueryBuilder.php
@@ -11,7 +11,7 @@ class QueryBuilder extends Object {
 
 	protected $_config = array();
 
-	protected $_autoConfig = array();
+	protected $_autoConfig = array('config', 'data');
 
 	public function __construct() {
 		parent::__construct();
--- test/cases/extensions/data/QueryBuilderTest.php
+++ test/cases/extensions/data/QueryBuilderTest.php
@@ -18,6 +18,54 @@ class QueryBuilderTest extends Unit {
 		});
 	}
 
+	public function testToDoesRespondToString() {
+		$query = $this->query;
+		$this->assertNotException('InvalidArgumentException', function() use($query) {
+			$query->to('string');
+		}
+	}
+
+	public function testImportReturnsThis() {
+		$expected = 'li3_charizard\extensions\data\QueryBuilder';
+		$this->assertInstanceOf($expected, $query->import(array()));
+	}
+
+	public function testUrgentCareFacilitySearch() {
+		$data = array(
+			'select' => array(
+				'name' => 'Urgent',
+			),
+			'fields' => array(
+				'facility_id',
+			),
+			'filter' => array(
+				'field' => array(),
+				'facet_query' => array(),
+			),
+			'sort' => array(
+				'geodist(geo,36.1537,-95.9926)' => 'asc',
+				'score' => 'desc',
+			),
+			'geo' => array(
+				'_distance_sort' => 1,
+				'field' => 'geo',
+				'latlong' => '36.1537,-95.9926',
+				'radius' => 10000,
+			),
+			'rows' => 10,
+			'offset' => 0,
+		);
+		$expected = 'select?wt=json&' +
+			'q=name:Urgent&' +
+			'start=0&' +
+			'rows=10&' +
+			'fl=facility_id,name&sort=geodist(geo,40.694599,-73.990638) asc,score desc&' +
+			'fq={!bbox pt=40.694599,-73.990638 sfield=geo d=10000}&' +
+			'defType=edismax';
+		$this->assertIdentical($expected, $query->import($data)->to('string'));
+	}
+
+
 }
 
 ?>
