d7059264b3b58306abb511b6039e672035bebf7f
Blaine Schmeisser <BlaineSch@gmail.com>
2013-11-05 17:18:08 -0600

    Fill out some of the easy part of the query builder.

--- extensions/data/QueryBuilder.php
+++ extensions/data/QueryBuilder.php
@@ -4,6 +4,7 @@ namespace li3_charizard\extensions\data;
 
 use lithium\core\Object;
 use InvalidArgumentException;
+use BadMethodCallException;
 
 class QueryBuilder extends Object {
 
@@ -30,7 +31,42 @@ class QueryBuilder extends Object {
 	}
 
 	public function __toString() {
-		return '';
+		$raw = array('select?wt=json');
+		foreach ($this->_data as $key => $value) {
+			$method = "_{$key}ToString";
+			$raw[] = $this->$method($value);
+		}
+		return implode('&', $raw);
+	}
+
+	protected function _startToString($value) {
+		return 'start=' . $value;
+	}
+
+	protected function _rowsToString($value) {
+		return 'rows=' . $value;
+	}
+
+	protected function _selectToString($values) {
+		foreach ($values as $key => &$value) {
+			$value = $key . ':' . $value;
+		}
+		return 'q=' . implode(' OR ', $values);
+	}
+
+	protected function _sortToString($values) {
+		foreach ($values as $key => &$value) {
+			if (is_numeric($key) && is_array($value)) {
+				$key = key($value);
+				$value = $value[$key];
+			}
+			$value = $key . ' '. $value;
+		}
+		return 'sort=' . implode(', ', $values);
+	}
+
+	public function __call($method, $values) {
+		// throw new BadMethodCallException("Method #{$method} not defined");
 	}
 
 }
